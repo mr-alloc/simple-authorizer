b80dcaa46728499b13638dfd159b348cae823870
mr-alloc <mr.allocater@gmail.com>
2025-03-07 17:05:11 +0900

    post: [OAuth 2.0] 스펙문서 작성중 - 권한부여 유형 설명.

---
+++ src/main/java/io/alloc/simepleauthorizer/SimepleAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package io.alloc.simepleauthorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimepleAuthorizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimepleAuthorizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/alloc/simepleauthorizer/SimepleAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alloc.simepleauthorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimepleAuthorizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
